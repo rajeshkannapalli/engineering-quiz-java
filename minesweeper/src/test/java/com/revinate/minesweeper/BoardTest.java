f186e4ddbc931c5c5a4a5409f2f0251b020ba811
Jorge Lee <jrg.lee@gmail.com>
2016-09-09 20:17:57 -0700

    Improve test with some description of the described scenarios

--- minesweeper/src/test/java/com/revinate/minesweeper/BoardTest.java
+++ minesweeper/src/test/java/com/revinate/minesweeper/BoardTest.java
@@ -11,14 +11,63 @@ public class BoardTest {
     public void smallBoardTest() throws Exception {
         Board board = new Board("1,1 2,2");
 
+        /*
+         * X = not revealed
+         * M = mine
+         * number = revealed
+         *
+         * X X X X X
+         * X M X X X
+         * X X M X X
+         * X X X X X
+         * X X X X X
+         */
+
         assertThat(board.reveal(0, 0), is(true));
         assertThat(board.isRevealed(0, 0), is(true));
         assertThat(board.getNumberOfSurroundingMines(0, 0), is(1));
+
+        /*
+         * 1 X X X X
+         * X M X X X
+         * X X M X X
+         * X X X X X
+         * X X X X X
+         */
 
         assertThat(board.isRevealed(1, 2), is(false));
         assertThat(board.reveal(1, 2), is(true));
         assertThat(board.isRevealed(1, 2), is(true));
         assertThat(board.getNumberOfSurroundingMines(1, 2), is(2));
+
+        /*
+         * 1 X X X X
+         * X M X X X
+         * X 2 M X X
+         * X X X X X
+         * X X X X X
+         */
+
+        assertThat(board.reveal(0, 3), is(true));
+        assertThat(board.isRevealed(0, 2), is(true));
+        assertThat(board.isRevealed(0, 3), is(true));
+        assertThat(board.isRevealed(0, 4), is(true));
+        assertThat(board.isRevealed(1, 2), is(true));
+        assertThat(board.isRevealed(1, 3), is(true));
+        assertThat(board.isRevealed(1, 4), is(true));
+        assertThat(board.isRevealed(2, 0), is(true));
+        assertThat(board.isRevealed(2, 1), is(true));
+        assertThat(board.isRevealed(2, 2), is(false));
+        assertThat(board.isRevealed(2, 3), is(true));
+        assertThat(board.isRevealed(2, 4), is(true));
+
+        /*
+         * 1 X 1 0 0
+         * X M 1 1 0
+         * 1 1 M 1 0
+         * 0 1 1 1 0
+         * 0 0 0 0 0
+         */
 
         assertThat(board.reveal(1, 1), is(false));
     }
